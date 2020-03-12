a5121dcd986b1c36dcfdf07b6bd76f47e8fd1733
Travis Gandy <travis_gandy@yahoo.com>
2020-03-12 18:33:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,8 +7,9 @@ def oxford_comma(array)
   when 3
     string = array[0] + ", " + array[1] + ", and " + array[2]
   else
-    string = Array.new
+    string = String.new
     array.each_with_index do |word, index|
+      puts string
       string += "and " + word if index == array.size - 1 
 end
  return string
