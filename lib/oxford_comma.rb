3f4338c0c10a6b3d6534ae4160215c6acc62c1e9
Travis Gandy <travis_gandy@yahoo.com>
2020-03-12 18:35:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,6 +10,7 @@ def oxford_comma(array)
     string = String.new
     array.each_with_index do |word, index|
       string += "and " + word if index == array.size - 1 
+      
 end
  return string
 end
